d02a958385889e45b61a4be1b46f207fea90e1fa
Hubert Mazur <01153193@pw.edu.pl>
2024-01-21 21:23:37 +0100

    Fix bug with missing else block in OnePointSplitCrosser

--- src/main/java/pl/edu/pw/ee/evo/operators/impl/OnePointSplitCrosser.java
+++ src/main/java/pl/edu/pw/ee/evo/operators/impl/OnePointSplitCrosser.java
@@ -25,14 +25,15 @@ public class OnePointSplitCrosser implements Crosser {
             if (random.nextDouble() > crossingProbability) {
                 results.getNonCrossedSpecimens().add(pair.first);
                 results.getNonCrossedSpecimens().add(pair.second);
+            } else {
+                int splitIndex = random.nextInt(gameVariant.getCodeLength() - 1) + 1;
+                var subSeq1 = pair.first.getCodeSequence().subList(0, splitIndex);
+                var subSeq2 = pair.second.getCodeSequence().subList(splitIndex, gameVariant.getCodeLength());
+                var child1 = new Specimen(new Code(gameVariant, Stream.concat(subSeq1.stream(), subSeq2.stream()).toList()));
+                var child2 = new Specimen(new Code(gameVariant, Stream.concat(subSeq2.stream(), subSeq1.stream()).toList()));
+                results.getOffsprings().add(child1);
+                results.getOffsprings().add(child2);
             }
-            int splitIndex = random.nextInt(gameVariant.getCodeLength() - 1) + 1;
-            var subSeq1 = pair.first.getCodeSequence().subList(0, splitIndex);
-            var subSeq2 = pair.second.getCodeSequence().subList(splitIndex, gameVariant.getCodeLength());
-            var child1 = new Specimen(new Code(gameVariant, Stream.concat(subSeq1.stream(), subSeq2.stream()).toList()));
-            var child2 = new Specimen(new Code(gameVariant, Stream.concat(subSeq2.stream(), subSeq1.stream()).toList()));
-            results.getOffsprings().add(child1);
-            results.getOffsprings().add(child2);
         }
         return results;
     }
